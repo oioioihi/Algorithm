80e532bd31e46d5f7d941381b38f345577f18642
Boeun's <rnqhdms@gmail.com>
2024-01-07 23:24:39 +0900

    two pointer : 백준 1940

--- testing/src/test/java/twoPointerTest.java
+++ testing/src/test/java/twoPointerTest.java
@@ -1,6 +1,11 @@
 import org.junit.jupiter.api.Test;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class twoPointerTest {
 
@@ -28,6 +33,34 @@ public class twoPointerTest {
         }
         System.out.println(count);
 
+    }
+
+    @Test
+    void twoPointer2() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(reader.readLine());
+        int m = Integer.parseInt(reader.readLine());
+        int count = 0;
+        int startIndex = 0;
+        int endIndex = n-1;
+
+        int[] a = new int[n];
+        StringTokenizer st = new StringTokenizer(reader.readLine());
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(a);
+
+        while(startIndex<endIndex){
+            if(a[startIndex] + a[endIndex] < m) startIndex++;
+            else if(a[startIndex] + a[endIndex] > m) endIndex--;
+            else{
+                count++;
+                startIndex++;
+                endIndex--;
+            }
+        }
+        System.out.println(count);
 
     }
 }
